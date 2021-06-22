8dfe644004f240dafe62354bc3d4ebb32caa8a84
AlvesFe <felipe.alves135@etec.sp.gov.br>
2021-06-22 16:21:46 -0300

    Adição dos metodos CRUD do formulário

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,41 +1,14 @@
-import { DebitContext } from 'context/DebitContext';
 import { SelectProps } from 'libs/FormRepository';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useContext } from 'react';
 import { Select as SelectInput } from './styles';
 
-export default function Select({ disabled, selected, type, onChange }: SelectProps) {
-
-  const { getUser } = useContext(DebitContext);
-  const [data, setData] = useState([] as Array<any>);
-
-  useEffect(() =>{
-    const getPropertiesSelect = async () => {
-      setData(await getUser());
-    }
-    getPropertiesSelect();
-  })
+export default function Select({ disabled, selected, data, onChange }: SelectProps) {
 
   return (
-    <>
-      {type === 'edit' && (
-        <SelectInput value={selected} disabled={disabled}>
-          <option value="0">Selecione</option>
-          {data && data.map((item) => (
-            <option key={item.id} value={item.id}>{item.name}</option>
-          ))}
-        </SelectInput>
-      )}
-      {type === 'create' && (
-        <SelectInput onChange={onChange} defaultValue={selected} disabled={disabled}>
-          <option value="0">Selecione</option>
-          {data && data.map((item) => (
-            <option key={item.id} value={item.id}>{item.name}</option>
-          ))}
-        </SelectInput>
-      )}
-    </>
-    // <></>
+    <SelectInput onChange={onChange} value={selected} disabled={disabled}>
+      <option value="0">Selecione</option>
+      {data && data.map((item) => (
+        <option key={item.id} value={item.id}>{item.name}</option>
+      ))}
+    </SelectInput>
   )
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -8,106 +8,128 @@ import { DebitContext } from 'context/DebitContext';
 import { FormProps } from 'libs/FormRepository';
 import { DebitProps } from 'libs/DebitRepository';
 
-export default function Form({type}: FormProps) {
-  const { data , activeId, createDebit } = useContext(DebitContext);
+export default function Form({ type }: FormProps) {
+  const { activeStringId, activeId, createDebit, getDebit, getUsers, updateDebit, deleteDebit, changeActiveId } = useContext(DebitContext);
   const [isEditable, setIsEditable] = useState(false);
-  const [debit, setDebit] = useState({} as DebitProps);
+  const [debit, setDebit] = useState({idUsuario:0, valor:0, motivo:""} as DebitProps);
+  const [data, setData] = useState([] as Array<any>);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if(type === "edit") toggleEditable(true);
+    if (type === "edit") toggleEditable(true);
     else toggleEditable(false);
-  },[type]);
+  }, [type]);
 
-  const addButtonAction = () => {
-    console.log(debit);
-  }
+  useEffect(() => {
+    const GetDataProps = async () => {
+      type === "edit" && setDebit(await getDebit(activeStringId));
+    }
+    GetDataProps();
+    setLoading(false);
+  },[getDebit, activeStringId, type])
+
+  useEffect(() => {
+    const getPropertiesSelect = async () => {
+      setData(await getUsers());
+    }
+    getPropertiesSelect();
+  })
+
+  useEffect(() => {
+    console.log("Debit: ",debit)
+  },[debit])
 
   const toggleEditable = (res: boolean) => {
     res ? setIsEditable(true) : setIsEditable(false);
   }
 
-  const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => { 
-    let newData = type === "edit" ? 
-      {...data[activeId-1], value: parseFloat(e.target.value)} : {...debit, value: parseFloat(e.target.value)};
+  const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    let newData = { ...debit, valor: parseFloat(e.target.value) };
     setDebit(newData);
   }
 
   const changeDescriptionValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    let newData = type === "edit" ?
-      {...data[activeId-1], description: e.target.value} : {...debit, description: e.target.value}
+    let newData = { ...debit, motivo: e.target.value };
     setDebit(newData);
   }
 
   const changeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     (e.target.value !== "0" && type === "create") ? toggleEditable(true) : toggleEditable(false);
-    const name = e.target[parseInt(e.target.value)].innerHTML;    
-    let newDebit = {...debit, id: parseInt(e.target.value), name};
+    const name = e.target[parseInt(e.target.value)].innerHTML;
+    let newDebit = {...debit, idUsuario: parseInt(e.target.value), name};
     setDebit(newDebit);
   }
 
   const handleCreateDebit = () => {
-    const keys = ["id","name", "description", "value"];
-    let itFailed = false;
-    
-    for(const key of keys){
-      if(!debit.hasOwnProperty(key)){
-        itFailed = true;
-      }
-    }
+    createDebit(debit);
+  }  
 
-    !itFailed && createDebit(debit);
+  const handlePatchDebit = () => {
+    updateDebit(debit, activeStringId);
   }
 
+  const handleDeleteDebit = () => {
+    deleteDebit(activeStringId);
+    changeActiveId("",0, true);
+    setDebit({idUsuario:0, valor:0, motivo:""});
+  }
+  
   return (
-    <FormContainer>
-      <Select
-        disabled={activeId !== 0}
-        selected={activeId}
-        type={type}
-        onChange={changeSelect}
-      />
-      <Input 
-        placeholder="Valor" 
-        onChange={changeInputValue} 
-        defaultValue={activeId !== 0 ? data[activeId - 1].value.toFixed(2) : ""} 
-        type="number" 
-        disabled={!isEditable}
-      />
-      <TextArea 
-        placeholder="Descrição/Motivo da dívida" 
-        onChange={changeDescriptionValue} 
-        defaultValue={activeId !== 0 ? data[activeId - 1].description : ""} 
-        disabled={!isEditable}
-      />
-
-      { type === 'edit' && (
-        <SectionButtons>
-          <Button
-            title="Excluir"
-            titleColor="var(--white)"
-            onClick={addButtonAction}
-            color="var(--secondary)"
+    <>
+      {((debit.hasOwnProperty('_id') || type === "create") && !loading) && (
+        <FormContainer>
+          <Select
+            disabled={activeId !== 0}
+            selected={debit.idUsuario}
+            data={data}
+            onChange={changeSelect}
           />
-          <Button
-            title="Salvar"
-            titleColor="var(--white)"
-            onClick={addButtonAction}
-            color="var(--primary)"
-          />
-        </SectionButtons>
-      )}
-
-      { type === 'create' && (
-        <SectionButtons>
-          <Button
-            title="Criar dívida"
-            titleColor="var(--white)"
-            onClick={isEditable ? handleCreateDebit : () => null}
-            color="var(--primary)"
+          <Input
+            placeholder="Valor"
+            onChange={changeInputValue}
+            value={debit.valor}
+            type="number"
             disabled={!isEditable}
           />
-        </SectionButtons>
+          <TextArea
+            placeholder="Descrição/Motivo da dívida"
+            onChange={changeDescriptionValue}
+            value={debit.motivo}
+            disabled={!isEditable}
+          />
+
+          {type === 'edit' && (
+            <SectionButtons>
+              <Button
+                title="Excluir"
+                titleColor="var(--white)"
+                onClick={isEditable ? handleDeleteDebit : () => null}
+                color="var(--secondary)"
+                disabled={!isEditable}
+              />
+              <Button
+                title="Salvar"
+                titleColor="var(--white)"
+                onClick={isEditable ? handlePatchDebit : () => null}
+                color="var(--primary)"
+                disabled={!isEditable}
+              />
+            </SectionButtons>
+          )}
+
+          {type === 'create' && (
+            <SectionButtons>
+              <Button
+                title="Criar dívida"
+                titleColor="var(--white)"
+                onClick={isEditable ? handleCreateDebit : () => null}
+                color="var(--primary)"
+                disabled={!isEditable}
+              />
+            </SectionButtons>
+          )}
+        </FormContainer>
       )}
-    </FormContainer>
+    </>
   )
 }
