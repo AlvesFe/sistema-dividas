23b5aa9127406c4139087283c9f9120e7bfe85a3
AlvesFe <felipe.alves135@etec.sp.gov.br>
2021-06-22 16:22:27 -0300

    Ajustes nas listagem para que se tornem dinamicas

--- src/components/List/ListItem/index.tsx
+++ src/components/List/ListItem/index.tsx
@@ -1,32 +1,50 @@
 import { DebitContext } from "context/DebitContext";
-import { ItemProps } from "libs/ListRepository";
+import { ItemProps, UserProps } from "libs/ListRepository";
 import { useContext } from "react";
 import { ItemContainer, ItemName, ItemDate, ItemValue } from "./styles";
 
 interface ListItemProps {
   data: ItemProps;
+  user: UserProps;
 }
 
-export default function ListItem({ data }: ListItemProps) {
+export default function ListItem({ data, user }: ListItemProps) {
   const { changeActiveId } = useContext(DebitContext);
 
-  const handleClick = (id: number)=>{
-    changeActiveId(id);
+  const handleClick = (id: string, idUsuario: number)=>{
+    changeActiveId(id, idUsuario);
   } 
 
+  const addZero = (num: number) => {
+    if (num <= 9) 
+        return "0" + num;
+    else
+        return num; 
+  }
+  
+  const Criado = new Date(data.criado);
+  const CriadoFormatted = (addZero(Criado.getDate())) + "/" + (addZero(Criado.getMonth() + 1) + "/" + Criado.getFullYear());
+
   return (
-    <ItemContainer onClick={() => handleClick(data.id)} >
-      <div>
-        <ItemName>
-          {data.name}
-        </ItemName>
-        <ItemDate>
-          {data.date}
-        </ItemDate>
-      </div>
-      <ItemValue>
-        R$ {data.value.toFixed(2)}
-      </ItemValue>
-    </ItemContainer>
+    <>
+      { (user && data) && (
+        <ItemContainer onClick={() => handleClick(data._id, data.idUsuario)} >
+        <div>
+          <ItemName>
+            {user.name}
+          </ItemName>
+          <ItemDate>
+            {
+              CriadoFormatted
+            }
+          </ItemDate>
+        </div>
+        <ItemValue>
+          R$ {data.valor.toFixed(2)}
+        </ItemValue>
+        </ItemContainer>
+      )}
+    </>
+    // <></>
   )
 }
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,15 +1,35 @@
-import { ListProps } from "libs/ListRepository";
+import { DebitContext } from "context/DebitContext";
+import { useEffect } from "react";
+import { useContext, useState } from "react";
 import ListItem from './ListItem'
 import { ListContainer } from "./styles";
 
-export default function List(props: ListProps) {
+export default function List() {
+  const { getDebits, getUsers, hasUpdated, toggleHasUpdated } = useContext(DebitContext);
+  const [data, setData] = useState([] as Array<any>);
+  const [users, setUsers] = useState([] as Array<any>);
 
-  const { data } : ListProps = props;
+  useEffect(() => {
+    const getPropertiesSelect = async () => {
+      setData(await getDebits());
+    }
+    if (hasUpdated) {
+      getPropertiesSelect();
+      toggleHasUpdated(false);
+    }
+  },[getDebits, hasUpdated, toggleHasUpdated]);  
+
+  useEffect(() => {
+    const getPropertiesSelect = async () => {
+      setUsers(await getUsers());
+    }
+    getPropertiesSelect();
+  },[getUsers]);
 
   return (
     <ListContainer>
-      { data && data.map((item) => (
-        <ListItem key={item.id} data={item} />
+      {data && data.map((item) => (
+        <ListItem key={item._id} data={item} user={users[item.idUsuario-1]} />
       ))}
     </ListContainer>
     // <></>
