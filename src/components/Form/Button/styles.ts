5f2cc3843236e429fb546b1327cbbafb4d487fa4
AlvesFe <felipe.alves135@etec.sp.gov.br>
2021-06-22 17:39:47 -0300

    ajuste na responsividade do componente de botão

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const FormButton = styled.div<FormButtonStyleProps>`
   display: flex;
   height: 3rem;
-  max-width: 30%;
+  max-width: 40%;
   padding: 1rem;
   border-radius: 5px;
   color: ${props => props.titleColor};
@@ -15,11 +15,15 @@ export const FormButton = styled.div<FormButtonStyleProps>`
   transition: box-shadow 0.3s ease-in-out;
   overflow: hidden;
 
+  @media(max-width: 1028px){
+    max-width: 100%;
+  }
+
   user-select: none;
   -moz-user-select: none;
   cursor: pointer;
 
-  ${props => !props.disabled && 
+  ${props => !props.disabled &&
     `
       &:hover{
         box-shadow: 0.5px 2px 5px rgba(0,0,0,0.25);
@@ -30,5 +34,5 @@ export const FormButton = styled.div<FormButtonStyleProps>`
     `
   }
   
-  ${ props => props.disabled && "background-color: rgba(0,0,0,0.25); cursor: default"}
+  ${props => props.disabled && "background-color: rgba(0,0,0,0.25); cursor: default"}
 `;
